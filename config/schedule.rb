028a116f2eaae63bb320d04a7fefa494774ee09d
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-07-04 17:30:43 +0800

    Fix: schedule.rb add some env var

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,6 +8,7 @@
 # set :output, "/path/to/my/cron_log.log"
 #
 # every 2.hours do
+#   # the following tasks are run in parallel (not in sequence)
 #   command "/usr/bin/some_great_command"
 #   runner "MyModel.some_method"
 #   rake "some:great:rake:task"
@@ -20,8 +21,18 @@
 # Learn more: http://github.com/javan/whenever
 
 env :PATH, ENV['PATH']
+set :environment, "development"
 set :output, 'log/rake.log'
+
 every 1.day, at: ['5.00 pm'] do
   rake "scraper:run"
+end
+
+every 1.day, at: ['5.04 pm'] do
   rake "scraper:seed:commit"
+end
+
+project_dir = `echo $PWD`.strip
+every 1.day, at: ['5.05 pm'] do
+  command "cd #{project_dir}; git push -u origin master; git push -u heroku master"
 end
