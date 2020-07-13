e650c873223b910752a2cf6be0a90f88eb8360e3
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-07-13 20:22:47 +0800

    Enable selected master

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "covid19-indo-harian"
 set :repo_url, "git@github.com:bandithijo/covid19-indo-harian.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
