015100bf4e76b8e0f6b7cabc6ceda5336e2558c7
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-07-15 06:54:12 +0800

    Fix block code indentation

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,5 +1,9 @@
 class Case < ApplicationRecord
+  after_create :calculate_todays_data
+
   validates :positif_covid, :sembuh_covid, :meninggal_covid, :jumlah_odp, :jumlah_pdp,
+            :positif_covid_today, :sembuh_covid_today, :meninggal_covid_today,
+            :jumlah_odp_today, :jumlah_pdp_today,
             presence: true,
             numericality: { only_integer: true}
   validates :fetched_at,
@@ -18,21 +22,20 @@ class Case < ApplicationRecord
     end
   end
 
-  after_create :calculate_todays_data
+  private
 
   def calculate_todays_data
-      kasus_today     = Case.all[-1]
-      kasus_yesterday = Case.all[-2]
-      # byebug
+    kasus_today     = Case.all[-1]
+    kasus_yesterday = Case.all[-2]
 
-      unless self.id == 1
-        self.update!(
-          positif_covid_today: (kasus_today.positif_covid - kasus_yesterday.positif_covid),
-          sembuh_covid_today: (kasus_today.sembuh_covid - kasus_yesterday.sembuh_covid),
-          meninggal_covid_today: (kasus_today.meninggal_covid - kasus_yesterday.meninggal_covid),
-          jumlah_odp_today: (kasus_today.jumlah_odp - kasus_yesterday.jumlah_odp),
-          jumlah_pdp_today: (kasus_today.jumlah_pdp - kasus_yesterday.jumlah_pdp),
-        )
-      end
+    unless self.id == 1
+      self.update!(
+        positif_covid_today: (kasus_today.positif_covid - kasus_yesterday.positif_covid),
+        sembuh_covid_today: (kasus_today.sembuh_covid - kasus_yesterday.sembuh_covid),
+        meninggal_covid_today: (kasus_today.meninggal_covid - kasus_yesterday.meninggal_covid),
+        jumlah_odp_today: (kasus_today.jumlah_odp - kasus_yesterday.jumlah_odp),
+        jumlah_pdp_today: (kasus_today.jumlah_pdp - kasus_yesterday.jumlah_pdp),
+      )
+    end
   end
 end
