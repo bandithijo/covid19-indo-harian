b606119fdb23ed528b517051a7c84ae422eedce4
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-07-17 18:12:13 +0800

    Correction some style with RuboCop

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Case < ApplicationRecord
   after_create :calculate_todays_data
 
@@ -5,19 +7,19 @@ class Case < ApplicationRecord
             :positif_covid_today, :sembuh_covid_today, :meninggal_covid_today,
             :jumlah_suspek_today, :jumlah_spesimen_today,
             presence: true,
-            numericality: { only_integer: true}
+            numericality: { only_integer: true }
   validates :fetched_at,
             presence: true
 
   def self.to_csv
-    attributes = %w(id fetched_at positif_covid meninggal_covid
-                    sembuh_covid jumlah_suspek jumlah_spesimen)
+    attributes = %w[id fetched_at positif_covid meninggal_covid
+                    sembuh_covid jumlah_suspek jumlah_spesimen]
 
-    CSV.generate(headers: true, col_sep: ";") do |csv|
+    CSV.generate(headers: true, col_sep: ';') do |csv|
       csv << attributes
 
       all.find_each do |kasus|
-        csv << attributes.map{ |attr| kasus.send(attr) }
+        csv << attributes.map { |attr| kasus.send(attr) }
       end
     end
   end
