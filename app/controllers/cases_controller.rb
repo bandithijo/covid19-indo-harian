0f90123707eb7bd1f17c8dad824d8eedd191288f
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-07-18 17:40:30 +0800

    Add today data each charts and add descrete true on every charts

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -19,11 +19,16 @@ class CasesController < ApplicationController
   end
 
   def chart
-    @data_positif_covid = Case.group_by_day(:fetched_at).sum(:positif_covid)
-    @data_meninggal_covid = Case.group_by_day(:fetched_at).sum(:meninggal_covid)
-    @data_sembuh_covid = Case.group_by_day(:fetched_at).sum(:sembuh_covid)
-    @data_jumlah_suspek = Case.group_by_day(:fetched_at).sum(:jumlah_suspek)
-    @data_jumlah_spesimen = Case.group_by_day(:fetched_at).sum(:jumlah_spesimen)
+    @data_positif_covid         = Case.group_by_day(:fetched_at).sum(:positif_covid)
+    @data_positif_covid_today   = Case.group_by_day(:fetched_at).sum(:positif_covid_today)
+    @data_meninggal_covid       = Case.group_by_day(:fetched_at).sum(:meninggal_covid)
+    @data_meninggal_covid_today = Case.group_by_day(:fetched_at).sum(:meninggal_covid_today)
+    @data_sembuh_covid          = Case.group_by_day(:fetched_at).sum(:sembuh_covid)
+    @data_sembuh_covid_today    = Case.group_by_day(:fetched_at).sum(:sembuh_covid_today)
+    @data_jumlah_suspek         = Case.group_by_day(:fetched_at).sum(:jumlah_suspek)
+    @data_jumlah_suspek_today   = Case.group_by_day(:fetched_at).sum(:jumlah_suspek_today)
+    @data_jumlah_spesimen       = Case.group_by_day(:fetched_at).sum(:jumlah_spesimen)
+    @data_jumlah_spesimen_today = Case.group_by_day(:fetched_at).sum(:jumlah_spesimen_today)
   end
 
   private
