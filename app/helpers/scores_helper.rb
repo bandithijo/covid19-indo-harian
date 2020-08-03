a31bd6e608d3c61bfe0351e498247ae0a141d78d
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-08-04 02:24:17 +0800

    Add: status helper; Change highlight color

--- app/helpers/scores_helper.rb
+++ app/helpers/scores_helper.rb
@@ -1,2 +1,15 @@
 module ScoresHelper
+  def status_badge(status)
+    if status == "RESIKO TINGGI"
+      '<span class="tag is-danger" style="width:100%;">RISIKO TINGGI</span>'
+    elsif status == "RESIKO SEDANG"
+      '<span class="tag is-orange" style="width:100%;">RISIKO SEDANG</span>'
+    elsif status == "RESIKO RENDAH"
+      '<span class="tag is-warning" style="width:100%;">RISIKO RENDAH</span>'
+    elsif status == "TIDAK ADA KASUS"
+      '<span class="tag is-success" style="width:100%;">TIDAK ADA KASUS</span>'
+    elsif status == "TIDAK TERDAMPAK"
+      '<span class="tag is-light" style="width:100%;">TIDAK TERDAMPAK</span>'
+    end
+  end
 end
