e83f92557c412b0dcfb4df125927976ea1bab0de
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-08-04 02:59:48 +0800

    Change: find to select for simple search

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -6,7 +6,7 @@ class ScoresController < ApplicationController
   def index
     if params[:search]
       unless params[:search].empty?
-        @data_zona = (Score.all.last.data.find { |kota| kota[params[:search].upcase] })&.split(/}\s*,\s*{/)
+        @data_zona = (Score.all.last.data.select { |data| data[params[:search].upcase] })
       else
         @data_zona = Score.all.last.data
       end
