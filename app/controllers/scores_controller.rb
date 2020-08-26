d2c879ed188a3f81933698774f19d6086e7f7a10
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-08-26 21:02:31 +0800

    Change @data_zona object

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -5,9 +5,9 @@ class ScoresController < ApplicationController
 
   def index
     if params[:search] || !params[:search].nil?
-      @data_zona = (Score.all.last.data.find_all { |data| data[params[:search].upcase] })
+      @data_zona = Score.all.last.data.map { |d| JSON.parse(d.gsub('=>', ':')) }.find_all { |d| d["kota"].include? params[:search].upcase }
     else
-      @data_zona = Score.all.last.data
+      @data_zona = Score.all.last.data.map { |d| JSON.parse(d.gsub('=>', ':')) }
     end
   end
 
