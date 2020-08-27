497d4e2de26ceacd8d8c3b4e9c5b5d6305918437
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-08-27 08:48:15 +0800

    Create logic busines for inser data prev on scores

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class Score < ApplicationRecord
-  serialize :metadata, Array
+  after_create :insert_data_prev
+
+  private
+
+  def insert_data_prev
+    unless id == 1
+      data_prev = Score.all.second_to_last.data.map { |d| JSON.parse(d.gsub('=>', ':')) }
+      update!(data_prev: data_prev)
+    end
+  end
 end
